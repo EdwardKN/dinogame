385faeef9fb57f975c3795c623ec9bac893ef625
EdwardKN <edward.klinghammer@gmail.com>
2020-03-28 20:31:18 +0000

    Logging in fully working and testing birds

--- Script/script.js
+++ Script/script.js
@@ -9,14 +9,17 @@ var dinoImg = new Image();
 
 var back1Img = new Image();
 var back2Img = new Image();
-back1Img.src = 'Images/back1.png';
-back2Img.src = 'Images/back2.png';
+back1Img.src = 'Images/Background/back1.png';
+back2Img.src = 'Images/Background/back2.png';
 
 var cactus1Img = new Image();
-cactus1Img.src = 'Images/cactus1.png';
+cactus1Img.src = 'Images/Cactus/cactus1.png';
 
 var cactus2Img = new Image();
-cactus2Img.src = 'Images/cactus2.png';
+cactus2Img.src = 'Images/Cactus/cactus2.png';
+
+var bird1Img = new Image();
+
 
 
 var jumpSound = new Audio('Sounds/Jump.wav');
@@ -32,20 +35,22 @@ var coinSound = new Audio('Sounds/Coin.mp3');
 
 var death = new Audio('Sounds/Death.mp3');
 
-var menumusic = new Audio('Sounds/MenuMusic.wav')
+var menumusic = new Audio('Sounds/MenuMusic.mp3')
 
 var standardHeight = 480;
 
-var userArray = "";
+var userArray = [];
 
-var plaintext = ""
+var lineSpace = 768/25;
+
+var leaderboardShow = false;
 
 var user = {
   highscore:getCookie("highscore") === -1 ? 0 : getCookie("highscore"),
-  username: undefined,
-  password: undefined,
-  mail: undefined,
-  loggedIn: false,
+  username: getCookie("username") === -1 ? undefined : getCookie("username"),
+  password: getCookie("password") === -1 ? undefined : getCookie("password"),
+  mail: getCookie("mail") === -1 ? undefined : getCookie("mail"),
+  loggedIn: getCookie("loggedIn") === -1 ? false : getCookie("loggedIn"),
   puttingInUsername: false,
   puttingInPassword: false,
   puttingInMail: false,
@@ -55,7 +60,10 @@ var user = {
   passwordColor: "white",
   usernameColor: "white",
   passwordInputVisual: "",
-  noAccountColor: "white"
+  noAccountColor: "white",
+  rememberMe: false,
+  encryptedPassword: undefined,
+  logOutColor: "white"
 };
 
 var dino = {
@@ -83,10 +91,15 @@ var dino = {
   doOnce: false
 };
 
-var back1 = {
-  x: 0,
-  y: 0,
-};
+
+if(user.loggedIn === "true"){
+  getScore();
+  sendScore(user.username, Math.floor(user.highscore), user.password, user.mail);
+  dino.inAir = false;
+  dino.died = false;
+  user.loggedIn = true;
+}
+
 var cactus1 = {
   x: 2000+Math.random()*1000,
   y: standardHeight
@@ -94,6 +107,19 @@ var cactus1 = {
 var cactus2 = {
   x: cactus1.x+(Math.random()*1000)+2000,
   y: standardHeight
+};
+
+
+var bird1 = {
+  x: 2000+Math.random()*1000,
+  y: standardHeight,
+  animationState: 1,
+  image:undefined
+};
+
+var back1 = {
+  x: 0,
+  y: 0,
 };
 var back2 = {
   x: 1365,
@@ -118,16 +144,31 @@ window.addEventListener('mousemove', function(event){
 window.addEventListener('keydown', function(event){
   console.log(event)
   if(event.key === "f"){
-    canvas.requestFullscreen();      
+    if(canvas.RequestFullScreen){
+        canvas.RequestFullScreen();
+    }else if(canvas.webkitRequestFullScreen){
+        canvas.webkitRequestFullScreen();
+    }else if(canvas.mozRequestFullScreen){
+        canvas.mozRequestFullScreen();
+    }else if(canvas.msRequestFullscreen){
+        canvas.msRequestFullscreen();
+    }else{
+        alert("This browser doesn't supporter fullscreen");
+    }
   }
-  if(event.key === " " && dino.inAir === false && dino.died === false){
+  if(event.code === "Space" && dino.inAir === false && dino.died === false && user.loggedIn === true){
     jump();
+    dino.stand = false;
   }
-  if(event.key === "Shift"){
+  if(event.key === "Shift" && user.loggedIn === true){
     crouch()  
   }
   if(event.key === "Enter" && dino.died === true){
     exitDeath();
+  }
+  if(event.key === "Tab" && user.loggedIn === true){
+    leaderboardShow = true;
+    getScore();
   }
   if(user.puttingInUsername === true && event.key === "Backspace"){
     user.usernameInput = user.usernameInput.slice(0, -1);
@@ -143,13 +184,15 @@ window.addEventListener('keypress', function(event){
   }
   if(user.puttingInPassword === true && event.key !== "Enter" && user.passwordInput.length < 20){
     user.passwordInput += event.key;
-    user.passwordInputVisual += "#"
+    user.passwordInputVisual += "•"
   }
 });
 window.addEventListener('keyup', function(event){
-  console.log(event)
   if(event.key === "Shift"){
     crouchEnd()  
+  }
+  if(event.key === "Tab" && user.loggedIn === true){
+    leaderboardShow = false;
   }
 });
 
@@ -160,25 +203,32 @@ function update(){
   c.clearRect(0,0,canvas.width,canvas.height);
 
   if(dino.animationState === 1){
-    dino.image = 'Images/dino1.png';
+    dino.image = 'Images/Dinosaur/dino1.png';
   }
   if(dino.animationState === 2){
-    dino.image = 'Images/dino2.png';
+    dino.image = 'Images/Dinosaur/dino2.png';
   }
   if(dino.animationState === 3){
-    dino.image = 'Images/dino3.png';
+    dino.image = 'Images/Dinosaur/dino3.png';
   }
   if(dino.animationState === 4){
-    dino.image = 'Images/dino4.png';
+    dino.image = 'Images/Dinosaur/dino4.png';
   }
   if(dino.animationState === 5){
-    dino.image = 'Images/dino5.png';
+    dino.image = 'Images/Dinosaur/dino5.png';
   }
   if(dino.animationState === 5){
-    dino.image = 'Images/dino5.png';
+    dino.image = 'Images/Dinosaur/dino5.png';
   }
   if(dino.animationState === 6){
-    dino.image = 'Images/dino6.png';
+    dino.image = 'Images/Dinosaur/dino6.png';
+  }
+
+  if(bird1.animationState === 1){
+    bird1.image = 'Images/Dinosaur/dino1.png';
+  }
+  if(bird1.animationState === 2){
+    bird1.image = 'Images/Dinosaur/dino2.png';
   }
 
 
@@ -205,6 +255,14 @@ function update(){
   }else {
     dinoImg.addEventListener('load', loaded)
     dinoImg.addEventListener('error', function() {
+    })
+  }
+
+  if(bird1Img.complete) {
+    c.drawImage(bird1Img, Math.floor(bird1.x), Math.floor(bird1.y));
+  }else {
+    bird1Img.addEventListener('load', loaded)
+    bird1Img.addEventListener('error', function() {
     })
   }
 
@@ -248,17 +306,17 @@ function update(){
   if(dino.y > standardHeight){
     dino.y = standardHeight;
   }
-  if(cactus1.x < -2000 && dino.backwards === false){
-    cactus1.x = Math.floor(3000+Math.random()*1000);
+  if(cactus1.x < -1000 && dino.backwards === false){
+    cactus1.x = Math.floor(1500+Math.random()*2000);
   }
-  if(cactus1.x > 2000 && dino.backwards === true){
-    cactus1.x = Math.floor(-3000-Math.random()*1000);
+  if(cactus1.x > 1000 && dino.backwards === true){
+    cactus1.x = Math.floor(-1500-Math.random()*2000);
   }  
-  if(cactus2.x < -3000 && dino.backwards === false){
-    cactus2.x = Math.floor(cactus1.x+(Math.random()*3000)+1000);
+  if(cactus2.x < -2000 && dino.backwards === false){
+    cactus2.x = Math.floor(1500+(Math.random()*2000)+1000);
   }
-  if(cactus2.x > 3000 && dino.backwards === true){
-    cactus2.x = Math.floor(cactus1.x-(Math.random()*3000)+1000);
+  if(cactus2.x > 2000 && dino.backwards === true){
+    cactus2.x = Math.floor(-1500-(Math.random()*2000)+1000);
   }  
   dinoImg.src = dino.image;
   if(user.highscore < 10){
@@ -341,7 +399,7 @@ function update(){
     }    
   }
   if(cactus1.x-800 < cactus2.x){
-    cactus2.x = Math.floor((Math.random()*3000)+1500);
+    cactus2.x = Math.floor((Math.random()*4000)+1500);
   }
   if(dino.visualShow === true){
     dino.scoreVisualVisual = "HI " + user.highscoreVisual + "   " + dino.scoreVisual;
@@ -359,14 +417,13 @@ function update(){
   c.font = "15px IMPACT";
   c.fillStyle = "black";
   c.fillText("V0.1 Experimental", 0, 768);
-  
-
 
   if(dino.died === false && dino.stand === false && user.loggedIn === true){
     back1.x -= dino.speed;
     back2.x -= dino.speed;
     cactus1.x -= dino.speed;
     cactus2.x -= dino.speed;
+    bird1.x -= dino.speed;
 
     dino.speed = Math.floor(dino.score/100+20);
 
@@ -419,23 +476,104 @@ function update(){
     deathMusic.play();
     runningMusic.pause();
     runningMusic.currentTime = 0;
+    c.fillRect(1365-420,768-120, 400, 100)
+    c.fillStyle = user.logOutColor;
+    c.textAlign = "center";
+    c.fillText("Log out", 1365-210, 768-30);
     if(dino.playOnce == false){
       death.play();
       dino.playOnce = true;
     }
+    if(mouse.x > 1365-420 && mouse.x < 1365-20 && mouse.y > 768-120 && mouse.y < 768-20){
+      console.log("Hej")
+      user.logOutColor = "#A2A2A2"
+      if(mouse.click === true){
+        document.cookie = `username=;Expires=Sun, 22 Oct 1970 08:00:00 UTC;`;
+        document.cookie = `password=;Expires=Sun, 22 Oct 1970 08:00:00 UTC;`;
+        document.cookie = `mail=;Expires=Sun, 22 Oct 1970 08:00:00 UTC;`;
+        document.cookie = `loggedIn=;Expires=Sun, 22 Oct 1970 08:00:00 UTC;`;
+        document.cookie = "highscore=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        user = {
+          highscore:0,
+          username: undefined,
+          password: undefined,
+          mail: undefined,
+          loggedIn: false,
+          puttingInUsername: false,
+          puttingInPassword: false,
+          puttingInMail: false,
+          loginColor: "white",
+          usernameInput:"",
+          passwordInput: "",
+          passwordColor: "white",
+          usernameColor: "white",
+          passwordInputVisual: "",
+          noAccountColor: "white",
+          rememberMe: false,
+          encryptedPassword: undefined,
+          logOutColor: "white"
+        };
+        exitDeath()
+      }
+    }else{
+      user.logOutColor = "white";
+    }
     if(user.highscore < dino.score){
       user.highscore = dino.score;
       document.cookie = `highscore=${user.highscore};Expires=Sun, 22 Oct 2030 08:00:00 UTC;`;
+      sendScore(user.username,Math.floor(user.highscore),user.password,user.mail);
     }
   }
+  if(leaderboardShow === true){
+        // Edvin får jobba här 
+        c.textAlign = "center"
+        c.fillStyle = "#1270CE";
+        c.fillRect(1365/20+10, 768/20+10, 1365-1365/10-20, 768-768/10-20);
+
+        c.fillStyle = "white";
+
+        c.fillStyle = "white";
+
+        c.font = " "+ 1365 / 30 + "px Impact, Sans-serif";
+
+        c.fillText("Leaderboard", 1365/2, lineSpace*3);
+
+        c.fillStyle = "#d4af37";
+        c.fillText("1." + userArray[0].name + ": " + userArray[0].score + " Points", 1365/2, lineSpace*5);
+        c.fillStyle = "#C0C0C0";
+        c.fillText("2." + userArray[1].name + ": " + userArray[1].score + " Points", 1365/2, lineSpace*7);
+        c.fillStyle = "#cd7f32";
+        c.fillText("3." + userArray[2].name + ": " + userArray[2].score + " Points", 1365/2, lineSpace*9);
+        c.fillStyle = "white";
+        c.fillText("4." + userArray[3].name + ": " + userArray[3].score + " Points", 1365/2, lineSpace*11);
+        c.fillText("5." + userArray[4].name + ": " + userArray[4].score + " Points", 1365/2, lineSpace*13);
+        c.fillText("6." + userArray[5].name + ": " + userArray[5].score + " Points", 1365/2, lineSpace*15);
+        c.fillText("7." + userArray[6].name + ": " + userArray[6].score + " Points", 1365/2, lineSpace*17);
+        c.fillText("8." + userArray[7].name + ": " + userArray[7].score + " Points", 1365/2, lineSpace*19);
+        c.fillText("9." + userArray[8].name + ": " + userArray[8].score + " Points", 1365/2, lineSpace*21);
+        c.fillText("10." + userArray[9].name + ": " + userArray[9].score + " Points", 1365/2, lineSpace*23);
+        // till hit
+    }
   if(user.loggedIn === false){
+    menumusic.play();
     c.fillStyle = "black";
     c.fillRect(100, 100, 1365-200, 768-200);
     c.fillStyle = "white";
     c.fillRect(1365/2-100, 575, 200, 65);
-    c.font = "55px IMPACT, Sans-serif";
-    c.fillStyle = user.loginColor;
+    c.fillStyle = "white";
+    c.fillRect(1365/3*2-120, 500, 100, 55);
+    c.font = "45px IMPACT, Sans-serif";
+    c.textAlign = "right";
+    c.fillStyle = "white"
+    c.fillText("Remember me:", 1365/3*2-150, 555);
     c.textAlign = "center";
+    c.fillStyle = "black"
+    if(user.rememberMe === true){
+      c.fillText("X", 1365/3*2-70, 555);
+    }
+    c.fillStyle = "red";
+    c.fillText("Buttons work bad in fullscreen.(Just so you know)", 1365/2, 200)
+    c.fillStyle = user.loginColor;
     c.fillText("Login", 1365/2, 635);
     c.fillStyle = user.usernameColor;
     c.fillText("Username:" + user.usernameInput, 1365/2, 275);
@@ -446,12 +584,27 @@ function update(){
     c.font = "15px IMPACT, Sans-serif";
     c.fillText("Click here to register an account", 1365/2, 670)
     c.textDecoration = "none";
+    if(mouse.click === true){
+      user.puttingInPassword = false;
+      user.passwordColor = "white";
+      user.puttingInUsername = false;
+      user.usernameColor = "white";
+    }
+    if(mouse.x > ((1365/3)*2)-120 && mouse.x < ((1365/3)*2)-20 && mouse.y > 500 && mouse.y < 555){
+      if(mouse.click === true){
+        if(user.rememberMe === false){
+          user.rememberMe = true;
+          return;
+        }
+        if(user.rememberMe === true){
+          user.rememberMe = false;
+          return;
+        }
+      }
+    }
     if(mouse.x > 1365/2-100 && mouse.x < 1365/2-100+200 && mouse.y > 575 && mouse.y < 575+65){
       user.loginColor = "#A2A2A2";
       if(mouse.click === true){
-        if(user.usernameInput === "admin" && user.passwordInput === "banan123"){
-          user.loggedIn = true;
-        }
         getScore();
       }
     }else{
@@ -510,6 +663,18 @@ function animation2(){
   }
   setTimeout(animation1, 110);
 }
+function animation3(){
+    if(bird1.animationState === 2){
+      bird1.animationState = 1;
+    }
+  setTimeout(animation4, 220);
+}
+function animation4(){
+    if(bird1.animationState === 1){
+      bird1.animationState = 2;
+    }
+  setTimeout(animation3, 220);
+}
 function crouch(){
   dino.crouch = true;
   if(dino.animationState === 1 || dino.animationState === 2 && dino.inAir === false){
@@ -557,7 +722,7 @@ function exitDeath(){
     y: standardHeight
   };
   cactus2 = {
-    x: cactus1.x+(Math.random()*2000)+2000,
+    x: cactus1.x+(Math.random()*3000)+2000,
     y: standardHeight
   };
   back2 = {
@@ -628,13 +793,43 @@ function getScore(){
     http.onreadystatechange=(e)=>{
         userArray = JSON.parse(http.responseText)
         for (let i = 0; i < userArray.length; i++){
-          let ciphertext = userArray[i].password
-          var bytes = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
-          var plaintext = bytes.toString(CryptoJS.enc.Utf8);
-          if(plaintext === user.passwordInput && userArray[i].name === user.usernameInput){
+          
+          user.encryptedPassword = CryptoJS.MD5(user.passwordInput).toString();
+
+          if(user.encryptedPassword === userArray[i].password && userArray[i].name === user.usernameInput || user.encryptedPassword === user.password && userArray[i].name === user.username ){
             user.loggedIn = true;
+            menumusic.pause();
+            menumusic.currentTime = 0;
+            user.username = userArray[i].name;
+            user.password = userArray[i].password;
+            user.mail = userArray[i].mail;
+            user.highscore = userArray[i].score;
+            if(user.rememberMe === true){
+              document.cookie = `username=${user.username};Expires=Sun, 22 Oct 2030 08:00:00 UTC;`;
+              document.cookie = `password=${user.password};Expires=Sun, 22 Oct 2030 08:00:00 UTC;`;
+              document.cookie = `mail=${user.mail};Expires=Sun, 22 Oct 2030 08:00:00 UTC;`;
+              document.cookie = `loggedIn=${user.loggedIn};Expires=Sun, 22 Oct 2030 08:00:00 UTC;`;
+            }
             }     
-          plaintext = "";
         };
     };
+};
+
+function showLeaderboard(){
+    if (leaderboardShow == true) {
+        leaderboardShow = false;
+        return;
+    };
+    if(leaderboardShow == false) {
+        leaderboardShow = true;
+        getScore();
+        return;
+    };
+};
+
+function sendScore(username, score, password, mail){
+  const http = new XMLHttpRequest();   
+  const url=`https://l2niipto9l.execute-api.eu-north-1.amazonaws.com/test/update?username=${username}&score=${score}&password=${password}&mail=${mail}`;
+  http.open("GET", url);
+  http.send();
 };
